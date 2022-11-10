5b5d4bdae0d7320795a5aad4953eb1d0f912e713
JYS <jyuns0710@gmail.com>
2022-11-11 02:25:23 +0900

    프로그래머스 스터디 2회차

--- 조이스틱 copy.js
+++ 조이스틱 copy.js
@@ -3,15 +3,11 @@ function solution(name) {
 
   // 위아래로 이동하는 횟수
   name.split("").forEach((v, i) => {
-    // console.log(v);
     let ascii = name.charCodeAt(i);
-    // console.log(ascii);
-    // console.log(v);
+
     if (ascii < 77) {
-      // console.log("위로" + (ascii - 65));
       answer += ascii - 65;
     } else {
-      // console.log("밑으로" + (91 - ascii));
       answer += 91 - ascii;
     }
   });
@@ -19,6 +15,7 @@ function solution(name) {
   let max = 0;
   let chain = 0;
   let chainLastIndex = 0;
+
   // 좌우로 이동하는 횟수
   name.split("").forEach((v, i) => {
     if (v == "A") {
@@ -38,12 +35,6 @@ function solution(name) {
   let goBack = name.length - 1 - max + chainFirstIndex - 1;
 
   goStraight < goBack ? (answer += goStraight) : (answer += goBack);
-
-  // console.log("S: " + goStraight + ", B: " + goBack);
-
-  // console.log("max: " + max);
-  // console.log("CLI: " + chainLastIndex);
-  // console.log("CFI: " + chainFirstIndex);
 
   return answer;
 }
